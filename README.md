dc2b859839b6bffe1b6a503bdea3c0d578dfb682
Nikolas Ambroziewicz <ambroziewicznikolas@wp.pl>
2023-03-11 16:03:01 +0100

    Init Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# FinSUM
+FinSUM is a Aplication to help you track yu
+
+## Frontend
+React App
+
+Install Dependency
+
+```bash
+cd frontend
+yarn
+```
+
+Start the App
+
+```bash
+yarn dev
+```
+
+## Backend
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      xs: '375px',
+      sm: '600px',
+      md: '900px',
+      lg: '1200px',
+      xl: '1536px',
+    },
+  },
+  plugins: [],
+}
