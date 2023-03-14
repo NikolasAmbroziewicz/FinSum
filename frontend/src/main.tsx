6f94bef2cdaffb7ea3ef5b1a9547cb6e63898751
Nikolas Ambroziewicz <ambroziewicznikolas@wp.pl>
2023-03-14 12:43:34 +0100

    Added React Querry

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ReactQueryWrapper from './api/ReactQueryWrapper';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from './App'
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ReactQueryWrapper>
+        <App />
+      </ReactQueryWrapper>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
