feb79374db7f5d68c6b628921ac54f5f91caa45a
Nikolas Ambroziewicz <ambroziewicznikolas@wp.pl>
2023-03-16 09:57:24 +0100

    Added HomePage, DashboardPage, NotFound Page and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,23 @@
+import { Routes, Route} from 'react-router-dom'
+
+import HomePage from './pages/home/HomePage';
+import DashboardPage from './pages/dashboard/DashboardPage';
+import LoginPage from './pages/auth/LoginPage';
+import RegisterPage from './pages/auth/RegisterPage';
+import NotFoundPage from './pages/404/NotFound';
+
 function App() {
-  return <div>hello</div>
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage />}/>
+      <Route path='auth/'>
+        <Route path='login' element={<LoginPage />}/>
+        <Route path='register' element={<RegisterPage />}/> 
+      </Route>
+      <Route path='/dashboard' element={<DashboardPage />}/>
+      <Route path='*' element={<NotFoundPage />}/>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
