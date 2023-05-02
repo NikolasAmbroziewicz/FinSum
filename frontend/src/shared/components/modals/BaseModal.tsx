1b47e3420d331fff59bde043d8b7a8013d56c7f0
Nikolas Ambroziewicz <ambroziewicznikolas@wp.pl>
2023-05-02 18:31:06 +0200

    Added BaseModal and hook to handle logic

---
+++ frontend/src/shared/components/modals/BaseModal.tsx
@@ -0,0 +1,49 @@
+import H2 from "src/shared/components/headers/H2"
+
+import { Position } from 'src/shared/components/headers/Header.types'
+
+import { AiOutlineClose } from 'react-icons/ai'
+
+
+interface IBaseModal {
+    isOpen: boolean
+    onClose: () => void,
+    title: string,
+    content: string | JSX.Element,
+    action?: JSX.Element
+}
+
+const BaseModal: React.FC<IBaseModal> = ({ isOpen, onClose, title, content, action }) => {
+    return (
+        <>
+        {
+            isOpen && (
+                <div className={`${isOpen ? 'fixed': 'none'} inset-0 z-100 p-4 overflow-x-hidden overflow-y-auto bg-sky-500/[.2]`}>
+                <div className="bg-white m-auto my-14 max-w-[600px] rounded-md">
+                    <div className="flex justify-between p-4 border-b-[1px]">
+                        <H2 position={Position.left}>
+                            {title}
+                        </H2>
+                        <button onClick={onClose}>
+                            <AiOutlineClose className="text-gray-600 text-xl hover:text-black"/>
+                        </button>
+                    </div>
+                    <div className="flex justify-center p-4">
+                        {content}
+                    </div>
+                    {
+                        action && (
+                            <div className="flex justify-between p-4 border-t-[1px]">
+                                {action}
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+            )
+        }
+        </>
+    )
+}
+
+export default BaseModal
