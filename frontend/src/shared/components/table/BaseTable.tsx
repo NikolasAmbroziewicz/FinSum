4c293fc86a0b384bef1c34ee74b72941da1098fb
Nikolas Ambroziewicz <ambroziewicznikolas@wp.pl>
2023-05-12 12:37:19 +0200

    Added Moment js and Fix styles in BaseTable

--- frontend/src/shared/components/table/BaseTable.tsx
+++ frontend/src/shared/components/table/BaseTable.tsx
@@ -1,15 +1,16 @@
 interface IBaseTable {
   headers: string[],
-  children: JSX.Element
+  children: JSX.Element[] | JSX.Element,
+  headerWidth: string[],
 }
 
-const BaseTable: React.FC<IBaseTable> = ({ headers, children }) => {
+const BaseTable: React.FC<IBaseTable> = ({ headers,headerWidth, children }) => {
   return (
     <table className="w-full border-2 border-gray-100 rounded-lg overflow-hidden">
       <thead>
         <tr>
           {
-            headers.map((header) => <th className="bg-gray-100 text-gray-600 text-left px-4 py-2">{header}</th>)
+            headers.map((header, index) => <th className={`bg-gray-100 text-gray-600 text-left px-4 py-2 w-[${headerWidth[index]}]`}>{header}</th>)
           }
         </tr>
       </thead>
