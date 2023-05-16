453fcb93b2ea89f496ed5015491e86b1564d8ce0
Nikolas Ambroziewicz <ambroziewicznikolas@wp.pl>
2023-05-16 12:58:32 +0200

    Added BaseButton color prop

--- frontend/src/shared/components/button/base/BaseButton.tsx
+++ frontend/src/shared/components/button/base/BaseButton.tsx
@@ -1,14 +1,23 @@
+import { ButtonTheme } from "./types";
+
 export interface IBaseButton {
   handler?: () => void;
   children: JSX.Element | string;
   type?: 'button' | 'submit' | 'reset' | undefined;
+  color?: ButtonTheme,
+  styles?: string
 }
 
-const BaseButton: React.FC<IBaseButton> = ({ handler, type, children }) => {
+const BaseButton: React.FC<IBaseButton> = ({ color = ButtonTheme.base, handler, type, styles, children }) => {
+
+  const buttonColor = () => {
+    return color === ButtonTheme.base ? 'bg-sky-500 hover:bg-sky-600' : 'bg-red-600 hover:bg-red-500'
+  }
+
   return (
     <button
       onClick={handler}
-      className="bg-sky-500 hover:bg-sky-600 text-white py-2 px-4 rounded"
+      className={`${buttonColor()} text-white py-2 px-4 rounded ${styles}`}
       type={type}
     >
       {children}
