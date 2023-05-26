ef2087848c893ac4056ce7af2fa0ab0e5dceed0b
Nikolas Ambroziewicz <ambroziewicznikolas@wp.pl>
2023-05-26 12:33:36 +0200

    Added Notification Context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
+import { NotificationProvider } from './context/NotificationContext';
+
 import { store } from 'src/store/main';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <NotificationProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </NotificationProvider>
   </React.StrictMode>
 );
