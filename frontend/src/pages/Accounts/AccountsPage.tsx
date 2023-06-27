38e4b726701375cc6a47fc4686846a5d18136708
Nikolas Ambroziewicz <ambroziewicznikolas@wp.pl>
2023-06-27 14:01:12 +0200

    Added AccountForm component

--- frontend/src/pages/Accounts/AccountsPage.tsx
+++ frontend/src/pages/Accounts/AccountsPage.tsx
@@ -1,7 +1,31 @@
-import {} from 'react'
+import { useEffect } from 'react'
+
+import BaseButton from 'src/shared/components/Button/base/BaseButton';
+import BaseModal from 'src/shared/components/Modals/BaseModal';
+import PageHeader from 'src/shared/components/PageHeader/PageHeader';
+
+import AccountsForm from 'src/features/Accounts/components/AccountsForm';
+
+import { useModal } from 'src/shared/components/Modals/hooks/useModal';
 
 const AccountsPage = () => {
-  return <div>Accounts</div>;
+  const { handleOpenModal, isOpen } = useModal();
+  
+  return (
+    <div className="flex flex-col">
+      <PageHeader 
+        title='Accounts'
+      >
+        <BaseButton handler={handleOpenModal}>Add Accounts</BaseButton>
+      </PageHeader>
+      <BaseModal
+        isOpen={isOpen}
+        onClose={handleOpenModal}
+        title="Add Account"
+        content={<AccountsForm onClose={handleOpenModal} />}
+      />
+    </div>
+  );
 };
 
 export default AccountsPage;
