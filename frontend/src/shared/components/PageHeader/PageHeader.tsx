edf452a9affc987c8e3909cddaead7a1426fc130
Nikolas Ambroziewicz <ambroziewicznikolas@wp.pl>
2023-06-27 12:57:33 +0200

    Create reusable PageHeader and add it to IncomePage

---
+++ frontend/src/shared/components/PageHeader/PageHeader.tsx
@@ -0,0 +1,31 @@
+import H1 from 'src/shared/components/Headers/H1';
+
+import Calendar from 'src/shared/components/Calendar/Calendar';
+
+import { Position } from 'src/shared/components/Headers/Header.types';
+
+interface IPageHeader {
+  children: JSX.Element,
+  startDate: Date,
+  setStartDate: (date: Date) => void,
+  title: string
+}
+
+const PageHeader: React.FC<IPageHeader> = ({ children, startDate, setStartDate, title }) => {
+  return (
+    <>
+      <div className="flex">
+        <H1 styles="my-4" position={Position.left}>
+          {title}
+        </H1>
+        <p className="m-auto mr-2 text-gray-600">Year:</p>
+        <Calendar startDate={startDate} setStartDate={setStartDate} />
+      </div>
+      <div className="flex justify-end">
+        {children}
+      </div>
+    </>
+  )
+}
+
+export default PageHeader
