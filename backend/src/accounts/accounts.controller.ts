d4df50a314f4218e1f966c8efa07ca5edf5b40a9
Nikolas Ambroziewicz <ambroziewicznikolas@wp.pl>
2023-06-29 09:49:54 +0200

    Added Accounts Controller and Service

--- backend/src/accounts/accounts.controller.ts
+++ backend/src/accounts/accounts.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { 
+  Controller,
+  Delete,
+  Post, 
+  Get, 
+  Put, 
+  Body,
+  Query
+} from '@nestjs/common';
+
+import { GetCurrentUser } from '../common/decorators/getCurrentUser.decorator';
+import { AccountsService } from './accounts.service';
+
+import { AccountsDto } from './accounts.dto';
+
+import { UserWithTokens } from '../auth/auth.type';
+import { AccountsResponse } from './accounts.type'
 
 @Controller('accounts')
-export class AccountsController {}
+export class AccountsController {
+  constructor(private accountsService: AccountsService) {}
+
+  @Post('v1/add-account')
+  addAccount(
+    @Body() account: AccountsDto,
+    @GetCurrentUser() user: UserWithTokens,
+  ): Promise<AccountsResponse> {
+    return this.accountsService.addAccounts(account, user)
+  }
+
+  @Get('v1/get-accounts')
+  getAccounts(
+    @GetCurrentUser() user: UserWithTokens
+  ): Promise<AccountsResponse[]> {
+    return this.accountsService.getAccounts(user)
+  }
+
+  @Delete('v1/delete-account')
+  deleteIncome(@Query('id') id: number): Promise<AccountsResponse> {
+    return this.accountsService.deleteAccount(Number(id))
+  }
+
+  @Put('v1/edit-account')
+  editIncome(@Body() income: AccountsDto, @Query('id') id: number) {
+    return this.accountsService.editAccount(income, Number(id))
+  }
+}
--- backend/src/accounts/accounts.service.ts
+++ backend/src/accounts/accounts.service.ts
@@ -1,4 +1,105 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+
+import { PrismaService } from '../prisma/prisma.service';
+
+import { AccountsDto } from './accounts.dto';
+
+import { UserWithTokens } from '../auth/auth.type';
+import { AccountsResponse } from './accounts.type'
 
 @Injectable()
-export class AccountsService {}
+export class AccountsService {
+  constructor(private prisma: PrismaService) {}
+
+  async addAccounts(
+    account: AccountsDto,
+    user: UserWithTokens
+  ): Promise<AccountsResponse> {
+    const { email } = user
+    const { title, currency } = account
+
+    const addedAccount = await this.prisma.account.create({
+      data: {
+        name: title,
+        currency: currency,
+        user: {
+          connect: {
+            email: email
+          }
+        }
+      },
+    })
+
+    return {
+      ...addedAccount
+    }
+  }
+
+  async getAccounts (
+    user: UserWithTokens
+  ): Promise<AccountsResponse[]>{
+    const { email } = user
+
+    const allAccounts = await this.prisma.account.findMany({
+      where: {
+        user: {
+          email: email
+        }
+      }
+    })
+
+    return allAccounts
+  }
+
+  async deleteAccount (
+    id: number
+  ): Promise<AccountsResponse> {
+    try {
+      const deleteAccount = await this.prisma.account.delete({
+        where: {
+          id: id
+        }
+      })
+
+      return {
+        ...deleteAccount
+      }
+    } catch(e: any) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2025') {
+          throw new ForbiddenException('Account does not exist');
+        }
+      }
+    }
+  }
+
+  async editAccount (
+    account: AccountsDto,
+    id: number
+  ): Promise<AccountsResponse> {
+    try {
+      const { title, currency } = account
+
+      const updatedAccounts = await this.prisma.account.update({
+        data: {
+          name: title,
+          currency: currency
+        },
+        where: {
+          id: id
+        }
+      })
+
+      return {
+        ...updatedAccounts
+      }
+    } catch (e: any) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2025') {
+          throw new ForbiddenException('Account does not exist');
+        }
+      }
+    }
+  }
+}
