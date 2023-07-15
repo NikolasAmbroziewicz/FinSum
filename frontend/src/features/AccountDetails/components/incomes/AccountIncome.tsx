a7b1f0381c3c35c7e427ab36f70604ebdff503b5
Nikolas Ambroziewicz <ambroziewicznikolas@wp.pl>
2023-07-15 21:19:49 +0200

    Added AccountIncomeForm to AccountIncome

--- frontend/src/features/AccountDetails/components/incomes/AccountIncome.tsx
+++ frontend/src/features/AccountDetails/components/incomes/AccountIncome.tsx
@@ -1,24 +1,43 @@
+import { useEffect } from 'react'
+import { useParams } from "react-router-dom"
+import { useDispatch } from "react-redux"
+
 import H2 from "src/shared/components/Headers/H2"
 import BaseButton from "src/shared/components/Button/base/BaseButton"
+import BaseModal from 'src/shared/components/Modals/BaseModal'
+import AccountIncomeForm from './AccountIncomeForm'
 
 import AccountIncomesList from "./AccountIncomesList"
 
+import { useModal } from 'src/shared/components/Modals/hooks/useModal'
+
 const AccountIncome = () => {
-  const handleAddIncome = () => {
-    console.log('add income')
-  }
+  const dispatch = useDispatch()
+  const params = useParams()
+  const { handleOpenModal, isOpen } = useModal()
 
   return (
     <div>
       <div className="flex justify-between items-center">
         <H2>Incomes</H2>
         <div className="flex gap-2">
-          <BaseButton handler={handleAddIncome}>
+          <BaseButton handler={handleOpenModal}>
             Add Income
           </BaseButton>
         </div>
       </div>
       <AccountIncomesList />
+      <BaseModal 
+        isOpen={isOpen}
+        onClose={handleOpenModal}
+        title="Add Income"
+        content={
+          <AccountIncomeForm 
+            onClose={handleOpenModal} 
+            editForm={false} 
+            account_id={Number(params['accountId'])} />
+        }
+      />
     </div>
   )
 }
---
+++ frontend/src/features/AccountDetails/components/incomes/AccountIncomeForm.tsx
@@ -0,0 +1,88 @@
+import { useEffect } from 'react'
+
+import BaseButton from 'src/shared/components/Button/base/BaseButton';
+import BaseInput from 'src/shared/components/Input/base/BaseInput';
+import IncomeFormCalendar from 'src/shared/components/Calendar/IncomeFormCalendar';
+
+import FormElement from 'src/shared/components/Form/FormElement';
+
+import { useAccountIncome } from 'src/features/AccountDetails/hooks/useAccountIncome';
+import { AccountDetailsIncomeSchemaType } from 'src/features/AccountDetails/validators/AccountDetailsIncomes'
+
+interface IAccountIncomeForm {
+  onClose: () => void;
+  editForm: boolean;
+  income?: AccountDetailsIncomeSchemaType;
+  account_id: number
+}
+
+const AccountIncomeForm: React.FC<IAccountIncomeForm> = ({ 
+  account_id,
+  editForm,
+  income,
+  onClose,
+}) => {
+  const {
+    handleAddIncome, 
+    handleEditIncome,
+    errors,
+    handleSubmit,
+    register,
+    setValue,
+    getValues
+  } = useAccountIncome({
+    onClose: onClose,
+  })
+
+  const handleDateValue = (val: Date) => {
+    setValue('date', val, { shouldValidate: true });
+  };
+
+  useEffect(() => {
+    if (editForm && income) {
+      setValue('id', income?.id);
+      setValue('title', income.title);
+      setValue('date', new Date(income.date));
+      setValue('amount', income.amount);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <form onSubmit={
+      handleSubmit(editForm ? 
+        (data) => handleEditIncome(data): 
+        (data) => handleAddIncome(data, account_id)
+      )}
+      className="flex flex-col gap-4 w-screen mx-4"
+    >
+      <FormElement value="Title" error={errors.title?.message}>
+        <BaseInput
+          id="name"
+          type="text"
+          placeholder="Title"
+          error={!!errors.title?.message}
+          formHandler={register('title')}
+        />
+      </FormElement>
+      <FormElement value="Amount" error={errors.amount?.message}>
+        <BaseInput
+          id="amount"
+          type="number"
+          placeholder="Amount"
+          error={!!errors.amount?.message}
+          formHandler={register('amount')}
+        />
+      </FormElement>
+      <FormElement value="Date" error={errors.date?.message}>
+        <IncomeFormCalendar
+          date={getValues('date')}
+          setDate={handleDateValue}
+        />
+      </FormElement>
+      <BaseButton type="submit">Submit</BaseButton>
+    </form>
+  )
+}
+
+export default AccountIncomeForm
